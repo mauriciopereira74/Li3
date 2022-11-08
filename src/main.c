983d62d1b771f973a716cb04d432f83428b30773
xDwake <gabi.alex.sil2002@gmail.com>
2022-11-08 16:41:03 +0000

    minor changes

--- src/main.c
+++ src/main.c
@@ -3,14 +3,19 @@
 #include "../includes/rides.h"
 #include "../includes/parser.h"
 #include "../includes/interpreter.h"
+#include "../includes/hashtables.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <string.h>
-
+ 
 
 int main(){
+    users_table = g_hash_table_new(g_str_hash, g_str_equal);
+    drivers_table= g_hash_table_new(g_str_hash, g_str_equal);
+    rides_table= g_hash_table_new(g_str_hash, g_str_equal); 
+
     char* line = malloc(sizeof(char) * LINE_SIZE);
-    command_interpreter(line);
+    parser(line);
     return 0;
 }
