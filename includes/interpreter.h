c52ebbeff6eb46fe3eedf192df37f04666aa169a
xDwake <gabi.alex.sil2002@gmail.com>
2022-11-16 10:42:19 +0000

    fixes

--- includes/interpreter.h
+++ includes/interpreter.h
@@ -1,3 +1,3 @@
 char* filename_changer(int filecounter);
-void debug_interpreter(char*filepointer, char* input);
+void file_writer(char*filepointer, char* input);
 void command_interpreter(char* line);
