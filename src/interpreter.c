4c9f899cb1ef1df2a1e540989bde9e9aa9b8bc02
xDwake <gabi.alex.sil2002@gmail.com>
2022-11-16 10:41:47 +0000

    fixes

--- src/interpreter.c
+++ src/interpreter.c
@@ -7,8 +7,7 @@
 #include "../includes/parser.h"
 #include "../includes/interpreter.h"
 
-#define filenametemplate "./Resultados/command%d_output.txt"
-
+#define filenametemplate "../Resultados/command%d_output.txt"
 
 char* filename_changer(int filecounter){
     char* ptr = malloc(sizeof(char)* strlen(filenametemplate));
@@ -18,15 +17,14 @@ char* filename_changer(int filecounter){
 }
 
 //DEBUG
-void debug_interpreter(char*filepointer, char* input){
-    printf("ardeu\n");
+void file_writer(char*filepointer, char* input){
     FILE* resultado = fopen(filepointer,"w");
     fprintf(resultado,"%s",input);
     fclose(resultado);
 }
 
 void command_interpreter(char* line){
-    FILE* commands_file = fopen("./entrada/commands.txt","r");
+    FILE* commands_file = fopen("../entrada/commands.txt","r");
     int output_counter = 0;
     char* filepointer, *input;
     while(fgets(line,LINE_SIZE,commands_file)){
@@ -36,9 +34,9 @@ void command_interpreter(char* line){
         if(atoi(querie_id) == 1){
             filepointer = filename_changer(output_counter); // altera o nome do ficheiro de output (COLOCA-O NA PASTA RESULTADOS)
             input = strdup(strsep(&line,FILE_CSV_DELIM)); // pega no restante texto após o numero da querie e passa à funçao de execuçao da querie (pode requerer parsing posterior dependendo da querie a executar)
-            debug_interpreter(filepointer,input); // funçao pra debug ---> pseudo "executa querie 1"
-            //execQuerie1(input, filepointer); ---> ideia é executar a querie e escrever no file de output tudo dentro da funçao
             
+            file_writer(filepointer,input); // funçao pra debug ---> pseudo "executa querie 1"
+            //execQuerie1(input, filepointer); ---> ideia é executar a querie e escrever no file de output tudo dentro da funçao
         }
         if(atoi(querie_id) == 2){
             
