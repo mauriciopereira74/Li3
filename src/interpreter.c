a094302f30eab4331ebe505841743e5b7e0e4f20
xDwake <gabi.alex.sil2002@gmail.com>
2022-11-20 23:14:20 +0000

    WORKING

--- src/interpreter.c
+++ src/interpreter.c
@@ -11,12 +11,12 @@
 #include "../includes/hashtables.h"
 
 
+
 #define filenametemplate "../Resultados/command%d_output.txt"
 
 char* filename_changer(int filecounter){
     char* ptr = malloc(sizeof(char)* strlen(filenametemplate));
     sprintf(ptr,filenametemplate,filecounter);
-    
     return ptr;
 }
 
@@ -27,7 +27,8 @@ void file_writer(char*filepointer, char* input){
     fclose(resultado);
 }
 
-void command_interpreter(char* line,int num_lines[]){
+void command_interpreter(char* line,int num_lines[],char* path){
+    
     FILE* commands_file = fopen("../entrada/commands.txt","r");
     int output_counter = 0;
     char* filepointer, *input;
@@ -36,11 +37,10 @@ void command_interpreter(char* line,int num_lines[]){
         char* querie_id = strdup(strsep(&line," ")); // analisa qual a querie a executar
         
         if(atoi(querie_id) == 1){
+        
             filepointer = filename_changer(output_counter); // altera o nome do ficheiro de output (COLOCA-O NA PASTA RESULTADOS)
             input = strdup(strsep(&line,FILE_CSV_DELIM)); // pega no restante texto após o numero da querie e passa à funçao de execuçao da querie (pode requerer parsing posterior dependendo da querie a executar)
-            if(g_hash_table_contains(users_table,input))profileU(input,num_lines[2],filepointer);
-            else if (g_hash_table_contains(drivers_table,atoi(input))) profileD(input,num_lines[2],filepointer);
-            
+            profile(input,num_lines[2],filepointer);
             
         }
         if(atoi(querie_id) == 2){
