b24948d4d81f1efd03c53e318b5926b07e2c966a
xDwake <gabi.alex.sil2002@gmail.com>
2022-11-20 23:14:35 +0000

    WORKING

--- includes/rideAvr.h
+++ includes/rideAvr.h
@@ -1,3 +1,7 @@
+#ifndef RIDEAVR_H
+#define RIDEAVR_H
 void precoMedioViagens_cidade(char* city,int N,char* filepointer);
 int check_dates(int day,int mon, int year,struct tm date);
 void precoMedioViagens_datas(char *datas,int N,char* filepointer);
+
+#endif
