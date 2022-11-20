2e66bb05e303ea60bc40ffed43a0645604890f12
xDwake <gabi.alex.sil2002@gmail.com>
2022-11-20 23:14:45 +0000

    WORKING

--- includes/interpreter.h
+++ includes/interpreter.h
@@ -1,3 +1,8 @@
+#ifndef INTERPRETER_H
+#define INTERPRETER_H
+
 char* filename_changer(int filecounter);
 void file_writer(char*filepointer, char* input);
-void command_interpreter(char* line,int num_lines[]);
+void command_interpreter(char* line,int num_lines[],char* path);
+
+#endif
